8581709e80b142351f5fa416d48549fc88adb631
pancine <pedrolucaspancine50@gmail.com>
2020-05-29 16:49:38 -0300

    DONE!

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Sudoky-with-backtracking-Python-
+# Resolving sudoku with backtracking
+Works in this way:
+![alt text](https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Backtracking-no-backjumping.svg/1024px-Backtracking-no-backjumping.svg.png)
+
+![alt text](https://gifdownload.net/wp-content/uploads/2019/01/backtracking-gif-.gif)
+
+## Functions
+```python
+creategame(FILE) #create game from FILE
+checkrow(game, row, num) #check if num is in row
+checkcolumn(game, column, num) #check if num in is column
+checksquare(game, square, num) #check if num is in 3x3 square
+whichsquare(row, column) #return which square based on (row,column)
+check(game, row, column, square, num) #check if num can be used
+next(row, column) #next position
+find0(game, coord=(0, 0)) #find next position with a 0
+complete(game) #return True if completed all the grid
+solving(game, row=0, column=0) #backtracking solution
+main() #run the solve
+```
+## Game in row
+[00][01][02][03][04][05][06][07][08]\
+[09][10][11][12][13][14][15][16][17]\
+[18][19][20][21][22][23][24][25][26]\
+[27][28][29][30][31][32][33][34][35]\
+[36][37][38][39][40][41][42][43][44]\
+[45][46][47][48][49][50][51][52][53]\
+[54][55][56][57][58][59][60][61][62]\
+[63][64][65][66][67][68][69][70][71]\
+[72][73][74][75][76][77][78][79][80]\
+
+## FILE
+Your game must be on this format:
+- 0 for no number
+- space between numbers
+- endline with ;
+
+##### source
+image: https://pt.wikipedia.org/wiki/Backtracking
+gif: https://gifdownload.net/backtracking-gif/
---
+++ sudoku.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+def creategame(FILE):
+    f = open(FILE, "r")
+    game = np.mat(f.read())
+    f.close()
+    return game
+
+def checkrow(game, row, num):
+    for x in range(9):
+        if game[row, x] == num:
+            return False
+    return True
+
+def checkcolumn(game, column, num):
+    for x in range(9):
+        if game[x, column] == num:
+            return False
+    return True
+
+def checksquare(game, square, num):
+    if square < 3:
+        if square == 0:
+            if num in game[0:3, 0:3]:
+                return False
+            return True
+        elif square == 1:
+            if num in game[0:3, 3:6]:
+                return False
+            return True
+        elif square == 2:
+            if num in game[0:3, 6:9]:
+                return False
+            return True
+    elif square < 6:
+        if square == 3:
+            if num in game[3:6, 0:3]:
+                return False
+            return True
+        elif square == 4:
+            if num in game[3:6, 3:6]:
+                return False
+            return True
+        elif square == 5:
+            if num in game[3:6, 6:9]:
+                return False
+            return True
+    else:
+        if square == 6:
+            if num in game[6:9, 0:3]:
+                return False
+            return True
+        if square == 7:
+            if num in game[6:9, 3:6]:
+                return False
+            return True
+        if square == 8:
+            if num in game[6:9, 6:9]:
+                return False
+            return True
+
+def whichsquare(row, column):
+    if row < 3:
+        if column < 3:
+            return 0
+        elif column < 6:
+            return 1
+        else:
+            return 2
+    elif row < 6:
+        if column < 3:
+            return 3
+        elif column < 6:
+            return 4
+        else:
+            return 5
+    else:
+        if column < 3:
+            return 6
+        elif column < 6:
+            return 7
+        else:
+            return 8
+
+def check(game, row, column, square, num):
+    x = checkrow(game, row, num)
+    y = checkcolumn(game, column, num)
+    z = checksquare(game, square, num)
+    if x and y and z:
+        return True
+    return False
+
+def next(row, column):
+    if (column + 1) > 8:
+        return (row + 1, 0)
+    return (row, column + 1)
+
+def find0(game, coord=(0, 0)):
+    row, column = coord
+    if game[row, column] != 0:
+        return find0(game, next(row, column))
+    return coord
+
+def complete(game):
+    if 0 in game:
+        return False
+    return True
+
+def solving(game, row=0, column=0):
+
+    if complete(game):
+        return True
+    for x in range(1, 10):
+        row, column = find0(game, (row, column))
+        if check(game, row, column, whichsquare(row, column), x):
+            game[row, column] = x
+            if solving(game):
+                return True
+            game[row, column] = 0
+    return False
+
+def main():
+    sudoku = creategame('Sudoku.txt')
+    solving(sudoku)
+    print(sudoku)
+
+main()
